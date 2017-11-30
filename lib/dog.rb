e9cd7dad476d28fb7d558b08f3b50274f5380af2
Breyanna Vass <breyanna.bvass@gmail.com>
2017-11-30 16:46:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,7 +24,12 @@ class Dog
 
   def save
     if self.id
-      
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO dogs (name, breed) VALUES (?,?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
     end
   end
 
